e7e0e492d5f6a6de330642bbd67dd1dcfabc55d8
raihanbulldog <raihanbulldog@gmail.com>
2024-02-16 01:39:35 +0700

    feat: client side

---
+++ client_side/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import FetchSource from './newsSource.jsx'
+import FetchNewsAll from './allNews.jsx'
+
+function App() {
+  
+
+  return (
+    <FetchNewsAll/>
+  )
+}
+
+export default App
---
+++ client_side/src/card.jsx
@@ -0,0 +1,13 @@
+
+
+
+// function Card(){
+//     return(
+//        <div className="card">
+//             <img className="card-image" src={profilePic} alt="profile picture"></img>
+//             <h2 className="card-title">Bro Code</h2>
+//             <p className="card-text">I make Youtube videos and play video games</p>
+//        </div> 
+//     );
+// }
+// export default Card
